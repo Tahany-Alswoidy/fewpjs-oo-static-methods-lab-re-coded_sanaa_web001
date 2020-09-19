7abfdd7ad473c4ee2abd3e1f8539b7beefda58b4
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-09-19 05:12:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 class Formatter {
   //add static methods here
+  static capitalize (myString){
+    return myString.charAt(0).toUpperCase() + myString.slice(1);
+  }
+  static sanitize(myString){
+    // let valids=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','-',"'",' ','1','2','3','4','5','6','7','8','9','0'];
+    // let strArray=myString.toLowerCase().split()
+    // return strArray.find(char=>valids.includes(char));
+    return myString.replace(/^[A-Za-z0-9' -]*$/g, '');
+  }
 }
